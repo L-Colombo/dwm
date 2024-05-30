d382079dc200575519b3d4fa5bcc9ef58b71e324
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-05-30 20:41:33 +0200

    fix:formatting

--- config.def.h
+++ config.def.h
@@ -4,15 +4,10 @@
 static const unsigned int borderpx = 3; /* border pixel of windows */
 static const unsigned int gappx = 12;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int systraypinning =
-    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
-          X */
-static const unsigned int systrayonleft =
-    0; /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0; /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2; /* systray spacing */
-static const int systraypinningfailfirst =
-    1; /* 1: if pinning fails, display systray on the first monitor, False:
-          display systray on the last monitor*/
+static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
@@ -45,10 +40,8 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints =
-    1; /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen =
-    1; /* 1 will force focus on the fullscreen window */
+static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -72,8 +65,7 @@ static const Layout layouts[] = {
   }
 
 /* commands */
-static char dmenumon[2] =
-    "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
     "dmenu_run", "-m",  dmenumon,  "-fn", dmenufont, "-nb", col_gray1, "-nf",
     col_gray3,   "-sb", col_green, "-sf", col_gray4, NULL};
@@ -120,22 +112,13 @@ static const Key keys[] = {
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY | ShiftMask,
-     XK_j,
-     movestack,
-     {.i = +1}}, // added via movestack patch
-    {MODKEY | ShiftMask,
-     XK_k,
-     movestack,
-     {.i = -1}},                                // added via movestack patch
+    {MODKEY | ShiftMask, XK_j, movestack, {.i = +1}}, // added via movestack patch
+    {MODKEY | ShiftMask, XK_k, movestack, {.i = -1}}, // added via movestack patch
     {MODKEY | ShiftMask, XK_Return, zoom, {0}}, // swapped with termcmd
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
     {MODKEY, XK_n, setlayout, {.v = &layouts[0]}}, // swapped with telegram
-    {MODKEY | ShiftMask,
-     XK_space,
-     setlayout,
-     {.v = &layouts[1]}}, // swapped with browserCmd
+    {MODKEY | ShiftMask, XK_space, setlayout, {.v = &layouts[1]}}, // swapped with browserCmd
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
     {MODKEY, XK_space, setlayout, {0}},
     {MODKEY, XK_o, togglefloating, {0}}, // swappe with setlayout &layout[1]
@@ -161,9 +144,16 @@ static const Key keys[] = {
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+    TAGKEYS(XK_1, 0) 
+    TAGKEYS(XK_2, 1) 
+    TAGKEYS(XK_3, 2) 
+    TAGKEYS(XK_4, 3) 
+    TAGKEYS(XK_5, 4) 
+    TAGKEYS(XK_6, 5) 
+    TAGKEYS(XK_7, 6) 
+    TAGKEYS(XK_8, 7)
+    TAGKEYS(XK_9, 8)
+    {MODKEY | ShiftMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
