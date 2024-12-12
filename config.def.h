0c19f920d1c7f10e22a4f5dcffc5e2f735edad04
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-12-12 09:24:18 +0100

    change font

--- config.def.h
+++ config.def.h
@@ -22,8 +22,8 @@ enum showtab_modes { showtab_never,
 static const int	showtab = showtab_auto;	/* Default tab bar show mode */
 static const int	toptab	= True;	/* False means bottom tab bar */
 
-static const char	*fonts[]	      = {"Iosevka Nerd Font Propo:size=14"};
-static const char	 dmenufont[]      = "Iosevka Nerd Font Propo:size=14";
+static const char	*fonts[]	      = {"UbuntuMono Nerd Font Propo:size=16"};
+static const char	 dmenufont[]      = "UbuntuMono Nerd Font Propo:size=16";
 static const char	 col_gray1[]      = "#222222";
 static const char	 col_gray2[]      = "#555555";
 static const char	 col_white[]      = "#ffffff";
