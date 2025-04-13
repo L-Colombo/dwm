43b85263058d2747b9bdfaf302903c040a36ba05
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-04-13 11:36:30 +0200

    change font

--- config.def.h
+++ config.def.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 2; /* border pixel of windows */
+static const unsigned int borderpx = 4; /* border pixel of windows */
 static const unsigned int gappx = 10;   /* gaps between windows */
 static const unsigned int snap = 12;    /* snap pixel */
 static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
           X */
 static const unsigned int systrayonleft =
-    0; /* 0: systray in the right corner, >0: systray on left of status text */
+    0;                                        /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2; /* systray spacing */
 static const int systraypinningfailfirst =
-    1; /* 1: if pinning fails, display systray on the first monitor, False:
-          display systray on the last monitor*/
+    1;                            /* 1: if pinning fails, display systray on the first monitor, False:
+                                     display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
@@ -29,13 +29,13 @@ enum showtab_modes {
 static const int showtab = showtab_auto; /* Default tab bar show mode */
 static const int toptab = True;          /* False means bottom tab bar */
 
-static const char *fonts[] = {"Iosevka Nerd Font Propo:size=18"};
-static const char dmenufont[] = "Iosevka Nerd Font Propo:size=18";
+static const char *fonts[] = {"Iosevka Nerd Font Propo:size=16"};
+static const char dmenufont[] = "Iosevka Nerd Font Propo:size=16";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#555555";
 static const char col_white[] = "#ffffff";
-static const char accent_color[] = "#fc0fc0";
-static const char accent_bar_color[] = "#3400ff";
+static const char accent_color[] = "#5f021f";
+static const char accent_bar_color[] = "#5f021f";
 static const char *colors[][3] = {
     /*                              fg                bg         border   */
     [SchemeNorm] = {col_white, col_gray1, col_gray2},
@@ -89,8 +89,12 @@ static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[]=", tile}, /* first entry is default */
     {"><>", NULL}, /* no layout function means floating behavior */
-    {"[M]", monocle},  {"|M|", centeredmaster}, {"[@]", spiral},
-    {"[\\]", dwindle}, {"[#]", tatami},         {">M<", centeredfloatingmaster},
+    {"[M]", monocle},
+    {"|M|", centeredmaster},
+    {"[@]", spiral},
+    {"[\\]", dwindle},
+    {"[#]", tatami},
+    {">M<", centeredfloatingmaster},
 };
 
 /* window following */
@@ -100,24 +104,24 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                                      \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
+#define TAGKEYS(KEY, TAG)                                        \
+  {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
+      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
+      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
       {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd)                                                             \
-  {                                                                            \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+#define SHCMD(cmd)                                       \
+  {                                                      \
+    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL } \
   }
 
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",  dmenumon,     "-fn", dmenufont, "-nb", col_gray1, "-nf",
-    col_white,   "-sb", accent_color, "-sf", col_gray1, NULL};
+    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf",
+    col_white, "-sb", accent_color, "-sf", col_white, NULL};
 
 /* scratchpad */
 static const char scratchpadname[] = "scratchpad";
