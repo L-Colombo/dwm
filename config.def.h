f385fa6625a8ea39691535e6193e398bc2119a23
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-05-03 14:24:45 +0200

    change font

--- config.def.h
+++ config.def.h
@@ -29,8 +29,8 @@ enum showtab_modes {
 static const int showtab = showtab_auto; /* Default tab bar show mode */
 static const int toptab = True;          /* False means bottom tab bar */
 
-static const char *fonts[] = {"Mononoki Nerd Font Propo:size=18"};
-static const char dmenufont[] = "Mononoki Nerd Font Propo:size=18";
+static const char *fonts[] = {"MartianMono Nerd Font Propo:size=18"};
+static const char dmenufont[] = "MartianMono Nerd Font Propo:size=18";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#555555";
 static const char col_white[] = "#ffffff";
