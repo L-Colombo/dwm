8cad2f89f8391c84b1cc5a2194638f79a092e5b0
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-05-05 11:58:04 +0200

    change font

--- config.def.h
+++ config.def.h
@@ -8,11 +8,11 @@ static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
           X */
 static const unsigned int systrayonleft =
-    0; /* 0: systray in the right corner, >0: systray on left of status text */
+    0;                                        /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 4; /* systray spacing */
 static const int systraypinningfailfirst =
-    1; /* 1: if pinning fails, display systray on the first monitor, False:
-          display systray on the last monitor*/
+    1;                            /* 1: if pinning fails, display systray on the first monitor, False:
+                                     display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
@@ -29,8 +29,8 @@ enum showtab_modes {
 static const int showtab = showtab_auto; /* Default tab bar show mode */
 static const int toptab = True;          /* False means bottom tab bar */
 
-static const char *fonts[] = {"JetBrains Mono NL:size=15"};
-static const char dmenufont[] = "JetBrains Mono NL:size=15";
+static const char *fonts[] = {"BlexMono Nerd Font Propo:size=18"};
+static const char dmenufont[] = "BlexMono Nerd Font Propo:size=18";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#555555";
 static const char col_white[] = "#ffffff";
@@ -91,8 +91,12 @@ static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[]=", tile}, /* first entry is default */
     {"><>", NULL}, /* no layout function means floating behavior */
-    {"[M]", monocle},  {"|M|", centeredmaster}, {"[@]", spiral},
-    {"[\\]", dwindle}, {"[#]", tatami},         {">M<", centeredfloatingmaster},
+    {"[M]", monocle},
+    {"|M|", centeredmaster},
+    {"[@]", spiral},
+    {"[\\]", dwindle},
+    {"[#]", tatami},
+    {">M<", centeredfloatingmaster},
 };
 
 /* window following */
@@ -102,24 +106,24 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                                      \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
+#define TAGKEYS(KEY, TAG)                                        \
+  {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
+      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
+      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
       {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd)                                                             \
-  {                                                                            \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+#define SHCMD(cmd)                                       \
+  {                                                      \
+    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL } \
   }
 
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",  dmenumon,     "-fn", dmenufont, "-nb", col_gray1, "-nf",
-    col_white,   "-sb", accent_color, "-sf", col_white, NULL};
+    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf",
+    col_white, "-sb", accent_color, "-sf", col_white, NULL};
 
 /* scratchpad */
 static const char scratchpadname[] = "scratchpad";
